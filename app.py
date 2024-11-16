b6e1cf18688e29535c08ae5ef62483a17aa6f5cb
jimmy chavez <jimy.chavez@pi.edu.co>
2024-11-15 21:09:25 -0500

    avance 2.2

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from http.cookiejar import debug
-from webbrowser import register
-
-from flask import  Flask
+from flask import Flask
 from config import  db, DATABASE_URI
-from routes.user_routers import user_bp
-from router.product_routes import product_bp
-from router.store_routes import store_bp
+from routes.user_routes import user_bp
 
-app = flask(__name__)
 
-# cofigurar la base de datos
+app = Flask(__name__)
+
+#Configurar la URI de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# inicializar la base de datos
 db.init_app(app)
-app-register_blueprint(user_bp)
-app-register_blueprint(product_bp)
+
+#Registrar los Blueprints de las rutas
+app.register_blueprint(user_bp)
 
 # crear las tablas si no existen
 with app.app_context() :
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
 
 
 
